905f3848d0dfb997d4ae51cbdc45130c735681f4
ismail safha <ismailsafha@gmail.com>
2023-02-02 00:58:11 +0100

    products to mongodb

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,10 +1,13 @@
 import User from "../../models/User";
 import data from "../../utils/data";
+import Product from "../../models/Product";
 import db from "../../utils/db";
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: "seeded successfully" });
 };
